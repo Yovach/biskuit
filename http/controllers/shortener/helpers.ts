2f5747b1343eaa97cfe81886577350c48d942b4f
Quentin <31007601+Yovach@users.noreply.github.com>
2021-02-02 21:16:55 +0100

    doc: add jsdoc at helpers

--- http/controllers/shortener/helpers.ts
+++ http/controllers/shortener/helpers.ts
@@ -3,14 +3,30 @@ import { randomToken, URL_CHARACTERS } from "../../../libs/helpers.ts";
 export interface ShortenedURL {
     token: string;
     destination: string;
-    expireAt?: Date;
+    expireAt?: number;
 }
 
 const shortenedURLs: ShortenedURL[] = [];
 
-export function createShortenedURL(destination: string, expireAt?: Date): ShortenedURL {
+/**
+ * Shorten an URL
+ * @param destination - URL destination
+ * @param expireAt - Expiration of temporary link
+ * @returns an object that contains all data about shortened URL
+ */
+export function shortenURL(destination: string, expireAt?: number | string): ShortenedURL {
     let data = shortenedURLs.find((item) => item.destination === destination);
     if (!data) {
+        
+        if (expireAt !== undefined) {
+            if (typeof expireAt === "string") {
+                expireAt = parseInt(expireAt, 10)
+            }
+            if (expireAt > Date.now()) {
+                expireAt = undefined;
+            }
+        }
+
         data = {
             destination,
             expireAt,
@@ -21,14 +37,19 @@ export function createShortenedURL(destination: string, expireAt?: Date): Shorte
     return data;
 }
 
-export function retrieveShortenedURL(token: string): ShortenedURL | undefined {
+/**
+ * Retrieve a destination URL from a shortcode
+ * @param shortcode - Shortcode that corresponds to a destination
+ * @returns an object that contains all data about shortened URL if valid or "undefined"
+ */
+export function retrieveURL(shortcode: string): ShortenedURL | undefined {
     const data = shortenedURLs.find((item) => {
-        if (item.token !== token) {
+        if (item.token !== shortcode) {
             return false;
         }
         // if there is a expiration
         if (item.expireAt !== undefined) {
-            return item.expireAt.getTime() > Date.now()
+            return item.expireAt > Date.now()
         }
         return true;
     });
