43ca36bc1954e83b456953ae47cb8254868087bc
Quentin <contact@yovach.dev>
2023-12-11 13:52:31 +0100

    remove Deno code

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Biskuit
-
-**A secure web service written in TypeScript powered by the Deno runtime.**
--- libs/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "https://deno.land/x/oak@v6.5.0/router.ts";
-
-export interface IRouteModule {
-    
-    
-    initRoutes(router: Router): void;
-}
