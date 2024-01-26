598a7beec64b4927549db8fd3412c185e9a1b288
Quentin <contact@yovach.dev>
2024-01-26 15:03:04 +0100

    feat: init website with pandacss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,12 @@
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { vstack } from '../styled-system/patterns';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class={vstack({ gap: "4" })}>
+      <span>Create your URL</span>
+      <form>
+      </form>
     </div>
   );
 };
