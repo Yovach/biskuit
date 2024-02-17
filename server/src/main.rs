a98b62bfd0914ff576272877e801295fbac7cf44
Quentin <contact@yovach.dev>
2024-02-17 23:30:49 +0100

    fix(cors): add cors to authorize everything

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,10 @@
 use axum::{
+    http::{HeaderValue, Method, header::{self}},
     routing::{get, post},
     Router,
 };
 use dotenvy::dotenv;
+use tower_http::cors::CorsLayer;
 use tracing::info;
 
 pub mod routes;
@@ -18,8 +20,13 @@ async fn main() {
     let app = Router::new()
         .route("/s/:id", get(routes::get_short_url))
         .route("/s/", post(routes::create_short_url))
-        .route("/login/", post(routes::login));
-
+        .route("/login/", post(routes::login))
+        .layer(
+            CorsLayer::new()
+                .allow_origin("*".parse::<HeaderValue>().unwrap())
+                .allow_methods([Method::GET, Method::POST])
+                .allow_headers([header::CONTENT_TYPE])
+        );
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!(target: "test", "listening on {}", listener.local_addr().unwrap());
